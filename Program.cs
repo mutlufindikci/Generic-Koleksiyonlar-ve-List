b6c511fabb888d6affd726c10fe2b47627bd993c
mutlufindikci <mutlufindikci@gmail.com>
2022-01-02 18:50:39 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generic_Koleksiyonlar_ve_List
+{
+    public class Users
+    {
+        string firstName;
+        string lastName;
+
+        public string FIRSTNAME { get => firstName; set => firstName = value; }
+        public string LASTNAME { get => lastName; set => lastName = value; }
+
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Users user = new Users();
+
+
+            List<Users> userList = new List<Users>();
+
+            userList.Add(new Users()
+
+            {
+                FIRSTNAME = "Mutlu",
+                LASTNAME ="Fındıkçı"
+            }
+            );
+
+
+            user.FIRSTNAME = "Ahmet";
+            user.LASTNAME = "Altın";
+
+            userList.Add(user);
+
+            userList.ForEach(name => Console.WriteLine(name.FIRSTNAME+" "+name.LASTNAME));
+
+
+            Console.ReadKey();
+        }
+    }
+}
